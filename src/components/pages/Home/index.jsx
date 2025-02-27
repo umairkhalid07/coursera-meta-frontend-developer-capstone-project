8d75fca184a80de71096af86327de42102bd4a7a
umair-khalid-brainx <umair.khalid@brainxtech.com>
2025-02-27 15:15:34 +0500

    root home component

---
+++ src/components/pages/Home/index.jsx
@@ -0,0 +1,18 @@
+// Components
+import Hero from "./Hero";
+import Specials from "./Specials";
+import Testimonials from "./Testimonials";
+import OurStory from "./OurStory";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Specials />
+      <Testimonials />
+      <OurStory />
+    </>
+  );
+};
+
+export default Home;
