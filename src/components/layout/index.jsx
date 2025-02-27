386f60a393662205a10a26661612f036e5a64246
umair-khalid-brainx <umair.khalid@brainxtech.com>
2025-02-27 15:14:47 +0500

    layout file

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,15 @@
+// Components
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
