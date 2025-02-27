5c824a7f4040d3a7cc72affe9cab64278d66011b
umair-khalid-brainx <umair.khalid@brainxtech.com>
2025-02-27 15:16:46 +0500

    coursera API not working, added own fake API

---
+++ src/utils/fakeAPI.js
@@ -0,0 +1,7 @@
+const fetchAPI = (date) => {
+  return ["17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00"];
+};
+
+const submitAPI = (formData) => true;
+
+export { fetchAPI, submitAPI };
