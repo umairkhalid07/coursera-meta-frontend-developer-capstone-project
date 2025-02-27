28057d705a438cb283e688bfcad981df62b6a37e
umair-khalid-brainx <umair.khalid@brainxtech.com>
2025-02-27 15:14:10 +0500

    separate pages object helper

---
+++ src/utils/pages.js
@@ -0,0 +1,11 @@
+const pages = {
+  home: { name: "Home", path: "/", anchorable: true },
+  about: { name: "About", path: "/about", anchorable: true },
+  menu: { name: "Menu", path: "/menu", anchorable: true },
+  bookings: { name: "Reservations", path: "/bookings", anchorable: true },
+  confirmedBooking: { name: "Confirmed Booking", path: "/confirmed-booking", anchorable: false },
+  orderOnline: { name: "Order Online", path: "/order-online", anchorable: true },
+  login: { name: "Login", path: "/login", anchorable: true },
+};
+
+export default pages;
